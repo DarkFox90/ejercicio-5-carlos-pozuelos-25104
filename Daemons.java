3482ff83325ae2b928fd525b960a62cc1bcd57bf
DarkFox90 <carlospozuelos3@gmail.com>
2025-10-04 22:37:15 -0600

    implementados constructores y metodo abstracto en ProcesoIO y Daemons

--- Daemons.java
+++ Daemons.java
@@ -1,3 +1,19 @@
-public class Daemons {
-    
+public class Daemons extends Proceso {
+   
+    private int inicio;
+    private int fin;
+
+    public Daemons(int pid, String nombre, int inicio, int fin) {
+        super(pid, nombre);
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+
+
+    @Override
+    public void ejecutarAccion() {
+        
+    }
 }
+
+
--- ProcesoIO.java
+++ ProcesoIO.java
@@ -1,3 +1,14 @@
-public class ProcesoIO {
-    
+public class ProcesoIO extends Proceso{
+    private String identificacion;
+    private Vista vista;
+
+    public ProcesoIO(int pid, String nombre, String identificacion, Vista vista) {
+        super(pid, nombre);
+    }
+
+
+    @Override
+    public void ejecutarAccion() {
+        
+    }
 }
