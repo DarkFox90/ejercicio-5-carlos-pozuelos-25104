f93b385d35e09bebac75c3322d50aae4535c87ec
DarkFox90 <carlospozuelos3@gmail.com>
2025-10-04 22:40:02 -0600

    creado Main

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+ * Universidad del Valle de Guatemala
+ * Programacion Orientada a Objetos
+ * Ejercicio 5: polimorfismo via herencia
+ * fecha de creación: 4 de octubre de 2025
+ * ultima modificación: 6 de octubre de 2025
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+
+}
